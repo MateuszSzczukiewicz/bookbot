ec84ab7634e9fae1bb835687297779d774b271f6
MateuszSzczukiewicz <szcmat6277@gmail.com>
2024-12-29 20:44:41 +0100

    added words count

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-def main():
-    path_to_file = "books/frankenstein.txt"
+path_to_file = "books/frankenstein.txt"
 
+
+def words_count(text):
+    words = text.split()
+    count = 0
+    for _ in words:
+        count += 1
+
+    return count
+
+
+def main():
     with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
+        print(words_count(file_contents))
 
 
 main()
